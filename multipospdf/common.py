f74dcbe2111854439824cd2feee2aa3f687f644e
msujas <kenneth.marshall@esrf.fr>
2026-06-01 11:16:28 +0200

    add common functions, rust extension

---
+++ multipospdf/common.py
@@ -0,0 +1,42 @@
+from .functions import MultiFile, ImagePoni, PoniYZ, PoniList
+import os
+from glob import glob
+
+def getyz(file): #assuming files are in format: x_dty125.40_dtz135.00_...
+    '''
+    function for getting y and z values from file name in common file structure.
+    Assumes file names are in format ..._dty124.52_dtz260.10_...
+    '''
+    basefile = os.path.splitext(os.path.basename(file))[0]
+    filesplit = basefile.split('_')
+
+    ypart = [f for f in filesplit if 'dty' in f][0]
+    zpart = [f for f in filesplit if 'dtz' in f][0]
+    ypos = float(ypart.replace('dty',''))
+    zpos = float(zpart.replace('dtz',''))
+    return ypos,zpos
+
+
+def getponilist(ponidir):
+    files = glob(f'{ponidir}/*.poni')
+    ponilist= []
+    for file in files:
+        try:
+            y,z = getyz(file)
+        except IndexError:
+            print(f"couldn't extract y and z positions for {file}. Skipping")
+            continue
+        ponilist.append(PoniYZ(file, y,z))
+    return PoniList(ponilist)
+
+def getIPlist(cbfdir,ponilist:PoniList):
+    files = glob(f'{cbfdir}/*.cbf')
+    iplist = []
+    for file in files:
+        try:
+            y,z = getyz(file)
+        except:
+            print(f"couldn't extract y and z positions for {file}. Skipping")
+            continue
+        iplist.append(ImagePoni(file, y,z,ponilist))
+    return MultiFile(iplist)
---
+++ multipospdf/rustext.py
@@ -0,0 +1,23 @@
+from .common import *
+from multipos_rustpy import integrate_rp
+import time
+
+def runintegration_rp(cbfdir, ponidir, tthmin:float, tthmax:float,tthbins:int, chimin:float, chimax:float, 
+                      chibins:int, pfactor:float=0.85, maskfile=None, savcakes=False,outsubdir='cakes',
+                        cakemaskfile=None):
+    '''
+    a function for collecting poni and cbf files. Interpolating in Python (SciPy), then integrating and averaging with 
+    a Rust extension
+    '''
+    t0 = time.time()
+    plist = getponilist(ponidir)
+    mf = getIPlist(cbfdir,plist)
+    ponisubdir = 'ponis_rp'
+    mf.saveAllPonis(ponisubdir)
+    
+    newponidir = f'{cbfdir}/{ponisubdir}'
+    print(f'ponis interpolated and saved to {newponidir}. Elapsed time {time.time()-t0:.2f} s')
+    print('running integration with Rust extension')
+    integrate_rp(cbfdir, newponidir, tthmin, tthmax,tthbins, chimin,chimax,chibins,pfactor, maskfile, savcakes,
+                 outsubdir, cakemaskfile)
+    print(f'finished. Total time elapsed: {time.time()-t0:.2f} s')
